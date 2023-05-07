ad3f0e579ca06063da1ae46da760387810d30945
AlexCraciun <56975249+alxcraciun@users.noreply.github.com>
2023-05-07 21:13:00 +0300

    Finished Project (70p)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <cstddef>
 #include <fstream>
 #include <iostream>
@@ -11,30 +12,59 @@ ofstream out("mergeheap.out");
 class Node
 {
 public:
-    int value;
-    Node* parent;
-    Node* left;
-    Node* right;
-    Node* child;
-    int degree;
-    bool marked;
+    int value = 0;
+    Node* parent = nullptr;
+    Node* left = this;
+    Node* right = this;
+    Node* child = nullptr;
+    int degree = 0;
+    bool marked = false;
 
-    Node();
     Node(const int);
     Node(const Node&);
+
+    void merge(Node&);
 
     Node& operator=(const Node&);
     friend ostream& operator<<(ostream&, const Node&);
 };
 
-Node::Node()
-    : value(0), parent(nullptr), left(this), right(this), child(nullptr), marked(false), degree(0) {}
-
 Node::Node(const int val)
-    : value(val), parent(nullptr), left(this), right(this), child(nullptr), marked(false), degree(0) {}
+{
+    this->value = val;
+}
 
 Node::Node(const Node& node)
     : value(node.value), parent(node.parent), left(node.left), right(node.right), child(node.child), marked(node.marked), degree(node.degree) {}
+
+void Node::merge(Node& other)
+{
+    Node* left = other.left;
+    Node* right = other.right;
+
+    left->right = right;
+    right->left = left;
+
+    if (this->child != nullptr)
+    {
+        Node* child = this->child;
+        Node* last = child->left;
+
+        other.right = child;
+        other.left = last;
+        last->right = &other;
+        child->left = &other;
+        other.parent = this;
+    }
+    else
+    {
+        this->child = &other;
+        other.parent = this;
+        other.left = &other;
+        other.right = &other;
+    }
+    this->degree++;
+}
 
 Node& Node::operator=(const Node& node)
 {
@@ -83,6 +113,7 @@ ostream& operator<<(ostream& out, const Node& node)
 class FibonacciHeap
 {
     int size;
+    Node* head;
     Node* maxNode;
 
 public:
@@ -90,14 +121,21 @@ public:
     FibonacciHeap(int, Node*);
 
     void insert(int);
+
     int extractMax();
-    
+    void removeMax();
+    void consolidate();
+    void link(Node*, vector<Node*>&);
+
     FibonacciHeap& operator+=(const FibonacciHeap&);
     FibonacciHeap& operator=(const FibonacciHeap&);
 };
 
 FibonacciHeap::FibonacciHeap()
-    : maxNode(nullptr), size(0) {}
+{
+    maxNode = nullptr;
+    size = 0;
+}
 
 FibonacciHeap::FibonacciHeap(int size, Node* node)
     : size(size), maxNode(node) {}
@@ -118,7 +156,9 @@ void FibonacciHeap::insert(int val)
         this->maxNode->left = node;
 
         if (this->maxNode->value < node->value)
+        {
             this->maxNode = node;
+        }
     }
     this->size++;
 }
@@ -128,14 +168,18 @@ FibonacciHeap& FibonacciHeap::operator+=(const FibonacciHeap& other)
     this->size += other.size;
 
     if (this->maxNode == nullptr)
+    {
         this->maxNode = other.maxNode;
+    }
 
-    if (this ->maxNode == nullptr || other.maxNode == nullptr)
+    if (this->maxNode == nullptr || other.maxNode == nullptr)
+    {
         return *this;
+    }
 
     Node* root1 = this->maxNode;
     Node* last1 = root1->left;
-    
+
     Node* root2 = other.maxNode;
     Node* last2 = root2->left;
 
@@ -144,16 +188,116 @@ FibonacciHeap& FibonacciHeap::operator+=(const FibonacciHeap& other)
     root2->left = last1;
     last1->right = root2;
 
-    if(other.maxNode->value > this->maxNode->value) 
-        this->maxNode=other.maxNode;
-
+    if (other.maxNode->value > this->maxNode->value)
+    {
+        this->maxNode = other.maxNode;
+    }   
     return *this;
+}
+
+void FibonacciHeap::removeMax()
+{
+    Node* left = this->maxNode->left;
+    Node* right = this->maxNode->right;
+    left->right = right;
+    right->left = left;
+}
+
+void FibonacciHeap::link(Node* node, vector<Node*>& next_roots)
+{
+    Node* next = node->right;
+    int d = node->degree;
+
+    while (next_roots[d] != nullptr)
+    {
+        Node* node2 = next_roots[d];
+        if (node->value < node2->value)
+        {
+            swap(node, node2);
+        }
+        node->merge(*node2);
+        next_roots[d] = nullptr;
+        d++;
+    }
+    next_roots[d] = node;
+    if (!next->marked)
+    {
+        next->marked = true;
+        this->link(next, next_roots);
+        next->marked = false;
+    }
+}
+
+void FibonacciHeap::consolidate()
+{
+    int height = ceil(log2(this->size)) + 1;
+    vector<Node*> next_roots(height);
+    for (int i = 0; i < height; i++)
+        next_roots.push_back(nullptr);
+
+    this->maxNode->marked = true;
+    this->link(this->maxNode, next_roots);
+    this->maxNode->marked = false;
+
+    this->maxNode = nullptr;
+    this->head = nullptr;
+    for (unsigned long long i = 0; i < next_roots.size(); i++)
+    {
+        if (next_roots[i] != nullptr)
+        {
+            if (this->head == nullptr)
+            {
+                this->head = next_roots[i];
+            }
+            Node* last = this->head->left;
+            next_roots[i]->right = head;
+            next_roots[i]->left = last;
+            head->left = next_roots[i];
+            last->right = next_roots[i];
+            next_roots[i]->parent = nullptr;
+            if (this->maxNode == nullptr || next_roots[i]->value > this->maxNode->value)
+            {
+                this->maxNode = next_roots[i];
+            }
+        }
+    }
+}
+
+int FibonacciHeap::extractMax()
+{
+    Node* root = this->maxNode;
+    if (root != nullptr)
+    {
+        if (root->child != nullptr)
+        {
+            Node* last = this->maxNode->left;
+            Node* lastChild = root->child->left;
+
+            root->child->left = this->maxNode;
+            last->right = root->child;
+            lastChild->right = root;
+            root->left = lastChild;
+
+            root->child->parent->child = nullptr;
+        }
+        this->removeMax();
+        if (root == root->right)
+        {
+            this->maxNode = nullptr;
+        }
+        else
+        {
+            this->maxNode = root->right;
+            this->consolidate();
+        }
+        this->size--;
+    }
+    return root->value;
 }
 
 int main()
 {
     int N, Q;
-    FibonacciHeap fh;
     vector<FibonacciHeap*> heaps;
 
     in >> N >> Q;
@@ -166,9 +310,13 @@ int main()
         in >> cer;
 
         if (cer == 2)
+        {
             in >> v1;
+        }
         else
+        {
             in >> v1 >> v2;
+        }
 
         if (cer == 1)
         {
